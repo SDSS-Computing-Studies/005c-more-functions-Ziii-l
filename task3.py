07ac4786d4d1704e3b107220ee947dddf649dd1a
Ziii-l <ziweil101@deltalearns.ca>
2021-03-10 13:20:25 -0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -14,8 +14,7 @@ assert hypotenuse(6,8) == 10
 (2 points)
 """
 
-
-def hypotenuse():
-    pass
-
+import math
+def hypotenuse(x,y):
+    return math.pow(math.pow(x,2)+math.pow(y,2),1/2)
 assert hypotenuse(6,8) == 10
